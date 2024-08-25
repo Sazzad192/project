76ce8a95cecf7ccb1bff1f35592ed0d7df70db98
Sazzad192 <sazadulbg130@gmail.com>
2024-08-25 15:46:40 +0600

    fixed the text issue

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,11 @@
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  subsets: ['latin'],
+  variable: '--font-poppins',
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"]
+});
 
 export const metadata = {
   title: "Skill Shoot",
@@ -11,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className}`}>
+      <body className={`${poppins.className} font-poppins`}>
         <main>
           {children}
         </main>
